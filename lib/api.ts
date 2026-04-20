980e300b16b38b6eecd51b6d60a1cd3683f17b5a
TommeZ <devmechat@gmail.com>
2026-04-20 06:30:06 +0100

    api update

--- lib/api.ts
+++ lib/api.ts
@@ -43,3 +43,53 @@ export async function deletePatient(id: number) {
 
   return res.json();
 }
+
+export async function getPatient(id: number) {
+  const res = await fetch(`http://localhost:8000/patients/${id}`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch patient");
+  }
+
+  return res.json();
+}
+
+export async function addPrescription(
+  patientId: number,
+  data: { medication: string },
+) {
+  const res = await fetch(
+    `http://localhost:8000/patients/${patientId}/prescriptions`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to add prescription");
+  }
+
+  return res.json();
+}
+
+export async function updatePrescriptionStatus(
+  prescriptionId: number,
+  status: string,
+) {
+  const res = await fetch(
+    `http://localhost:8000/patients/prescriptions/${prescriptionId}?status=${status}`,
+    {
+      method: "PATCH",
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to update status");
+  }
+
+  return res.json();
+}
